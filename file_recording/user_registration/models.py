1eefb13dd320c209b15e65b470056e80d8cb8c1e
Parth Verma <v.parth98@gmail.com>
2017-07-24 17:02:41 +0530

    changed conditions on model fields

--- file_recording/user_registration/models.py
+++ file_recording/user_registration/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField()
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(max_length=10, unique=True)
     email = models.CharField(max_length=50)
     address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
