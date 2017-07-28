eaef3b98ed7909ed4619f2f011b005a75fc9b4e1
Parth Verma <v.parth98@gmail.com>
2017-07-28 22:43:04 +0530

    Added end point for registration detail of user

--- file_recording/registration/serializer.py
+++ file_recording/registration/serializer.py
@@ -2,9 +2,35 @@
 from rest_framework.serializers import ModelSerializer
 
 from file_recording.registration.models import Registration
+from file_recording.schemes.models import Flat
+from file_recording.schemes.models import Scheme
+from file_recording.schemes.serializers import FlatTypeSerializer
+
+
+class RegistrationSchemeSerializer(ModelSerializer):
+    class Meta:
+        model = Scheme
+        exclude = ('id', 'created_at', 'updated_at')
+
+
+class RegistrationFlatSerializer(ModelSerializer):
+    flat_type = FlatTypeSerializer()
+    scheme = RegistrationSchemeSerializer()
+
+    class Meta:
+        model = Flat
+        fields = '__all__'
 
 
 class RegistrationWriteSerializer(ModelSerializer):
+    class Meta:
+        model = Registration
+        exclude = ('id',)
+
+
+class RegistrationReadSerializer(ModelSerializer):
+    flat = RegistrationFlatSerializer()
+
     class Meta:
         model = Registration
         exclude = ('id',)
--- file_recording/registration/views.py
+++ file_recording/registration/views.py
@@ -4,22 +4,32 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
 from file_recording.constants.response_obj import ReturnObj
+from file_recording.registration.models import Registration
+from file_recording.registration.serializer import RegistrationReadSerializer
 from file_recording.registration.serializer import RegistrationWriteSerializer
 from file_recording.user.models import User
 # Create your views here.
 
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 @parser_classes((JSONParser,))
 def user_application(request):
-    user = User.objects.get(uid=request.data['user'])
-    request.data['user'] = user.id
-    serializer = RegistrationWriteSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return_obj = ReturnObj().ret(201)
-        return_obj['content']['result']['application'] = serializer.data
-    else:
+    if request.method == 'POST':
+        user = User.objects.get(uid=request.data['user'])
+        request.data['user'] = user.id
+        serializer = RegistrationWriteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return_obj = ReturnObj().ret(201)
+            return_obj['content']['result']['application'] = serializer.data
+        else:
+            return_obj = ReturnObj().ret(200)
+            return_obj['content']['result']['error'] = serializer.errors
+    elif request.method == 'GET':
+        uid = request.query_params.get('uid')
+        user = User.objects.get(uid=uid)
+        registrations = Registration.objects.filter(user=user)
+        serializer = RegistrationReadSerializer(registrations, many=True)
         return_obj = ReturnObj().ret(200)
-        return_obj['content']['result']['error'] = serializer.errors
+        return_obj['content']['result']['registrations'] = serializer.data
     return Response(data=return_obj['content'], status=return_obj['status'])
