88cfff1ada1e76577e750b6704f987349b6d8083
Parth Verma <v.parth98@gmail.com>
2017-07-29 15:25:42 +0530

    admin login

--- file_recording/employee/url.py
+++ file_recording/employee/url.py
@@ -2,7 +2,9 @@
 from django.conf.urls import url
 
 from file_recording.employee.views import create_admin
+from file_recording.employee.views import login_employee
 
 urlpatterns = [
-    url(r'create', create_admin),
+    url(r'create$', create_admin),
+    url(r'login$', login_employee)
 ]
--- file_recording/employee/views.py
+++ file_recording/employee/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.decorators import parser_classes
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
+from file_recording.constants.response_obj import ReturnObj
 from file_recording.employee.models import Admin
 from file_recording.employee.serializer import AdminSerializer
 
@@ -16,5 +18,29 @@ def create_admin(request):
         serializer.save()
     else:
         print(serializer.errors)
+
+
+@api_view(['POST'])
+@parser_classes((JSONParser,))
+def login_employee(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    employee = None
+    try:
+        employee = Admin.objects.get(email=email)
+    except Admin.DoesNotExist:
+        return_obj = ReturnObj().ret(401)
+        return_obj['content']['result']['message'] = 'No user with the given username exists.'
+        return Response(data=return_obj['content'], status=return_obj['status'])
+
+    if employee:
+        if employee.password == password:
+            return_obj = ReturnObj().ret(200)
+            return_obj['content']['result']['message'] = 'Login Successful'
+        else:
+            return_obj = ReturnObj().ret(401)
+            return_obj['content']['result']['message'] = 'Incorrect Password.'
+
+    return Response(data=return_obj['content'], status=return_obj['status'])
 
 # Create your views here.
